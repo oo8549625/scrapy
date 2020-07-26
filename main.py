a3c94f8c9fab05e153eabb13fdb87bcf5913fe9e
YELIWEI <a0970366492@gmail.com>
2020-07-26 21:55:38 +0800

    update

--- main.py
+++ main.py
@@ -132,4 +132,3 @@ if __name__ == '__main__':
 if __name__ != '__main__':
     gunicorn_logger = logging.getLogger('gunicorn.info')
     app.logger.handlers = gunicorn_logger.handlers
-    app.logger.setLevel(gunicorn_logger.level)
